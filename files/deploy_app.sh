cb4cc5479d06e657cb74b5125826a3183d5cb7fa
Abe, Paul Vince <r2upha@fanniemae.com>
2021-04-07 06:56:55 +0000

    Walang H sa Nicolas Cage shunga

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -4,13 +4,13 @@
 
 cat << EOM > /var/www/html/index.html
 <html>
-  <head><title>Meow!</title></head>
+  <head><title>CAAAAGGGEEE!</title></head>
   <body>
   <div style="width:800px;margin: 0 auto">
 
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
-  <center><h2>Nicholas Cage Appreciation Site</h2></center>
+  <center><h2>Nicolas Cage Appreciation Site</h2></center>
   C'mon ${PREFIX}'s app; let's get<br />
   <marquee><h1>S I C K E N I N G</h1></marquee>
   <!-- END -->
